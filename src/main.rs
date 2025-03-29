b5469255bf220df688ae51bc117dc12aeeeec18b
cazzalles <cazzalles@gmail.com>
2025-03-29 17:44:23 -0300

    Feat: conversation loop

--- src/main.rs
+++ src/main.rs
@@ -8,26 +8,30 @@ async fn main() {
 
     let model = "deepseek-r1:1.5b".to_string();
     let mut s = String::new();
-    
-    print!("Enter your prompt: ");
-    
-    let _ = stdout().flush();
-    
-    stdin().read_line(&mut s).expect("Failed to read line");
+    let mut prompt;
+    let mut res; 
 
-    if let Some('\n') = s.chars().next_back() {
-        s.pop();
-    }
+    while s != "exit" {    
+        print!("\nEnter your prompt (type 'exit' to quit): ");
+        
+        let _ = stdout().flush();
+        
+        stdin().read_line(&mut s).expect("Failed to read line");
 
-    if let Some('\r') = s.chars().next_back() {
-        s.pop();
-    }
+        if let Some('\n') = s.chars().next_back() {
+            s.pop();
+        }
 
-    let prompt = s.to_string();
-    let res = ollama.generate(GenerationRequest::new(model, prompt)).await;
+        if let Some('\r') = s.chars().next_back() {
+            s.pop();
+        }
 
-    if let Ok(res) = res {
-        println!("{}", res.response);
+        prompt = s.to_string();
+        res = ollama.generate(GenerationRequest::new(model.clone(), prompt.clone())).await;
+
+        if let Ok(res) = res {
+            println!("{}", res.response);
+        }
     }
 
 }
